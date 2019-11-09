da87b5a7cc587d84019142aa486a5221ce33d553
Calvin Glisson <cglis001@ucr.edu>
2019-11-09 10:41:15 -0800

    Yes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 
 int main() {
-    initscr();
-    cbreak();
-    noecho();
+    initscr(); //initialize
+    cbreak(); //don't wait for the user to hit enter
+    noecho(); //don't show the user's input
 
+    wrefresh(stdscr);
+    int ch = getch();
 
-    endwin();
+    endwin(); //end
     return 0;
 }
